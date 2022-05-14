8eb1f116825f8eba10a92409a1c24b9bfc22e8f3
Michele Ascoli <micheleascoli17@gmail.com>
2022-05-14 11:34:53 -0500

    Consumindo a API: Retrofit, Coroutines e LiveData.

---
+++ app/src/main/java/me/dio/bankline/ui/statement/BankStatementViewModel.kt
@@ -0,0 +1,11 @@
+package me.dio.bankline.ui.statement
+
+import androidx.lifecycle.ViewModel
+import me.dio.bankline.data.BanklineRepository
+
+class BankStatementViewModel : ViewModel() {
+
+    fun findBankStatement(accountHolderId: Int) =
+        BanklineRepository.findBanStatement(accountHolderId)
+
+}
